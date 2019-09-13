66b5b595eec73ccee0927d3d5689e281930a2c13
odogemo <odogemo@oalia-dev.com>
2019-09-13 17:55:21 +0200

    first commit fin de lecon 4 installation angular

---
+++ server/app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+mongoose.connect('mongodb+srv://toto:789456aze@angulardyma-mczpu.mongodb.net/test?retryWrites=true&w=majority'
+,{ 
+    keepAlive:true,
+    reconnectTries: Number.MAX_VALUE,
+    useNewUrlParser: true 
+  } ,(err)=>{
+  if(err){
+    console.log('Connection erreur : '+ err);
+  }else{
+    console.log("On est connecté");
+  }
+})
+
+// * pour récupérer toutes les requetes entrantes
+// res pour response 
+// le but est de retourné notre application angular quelque soit la requete entrantes 
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname, '../public/index.html'))// Notre application angular est situé dans notre dossier public un cran au dessus
+});
+
+
+module.exports = app;
