773a6ec114c11254c4ea3284554e2d056e6798d1
odogemo <odogemo@oalia-dev.com>
2019-09-25 17:49:39 +0200

    création d'un token avec clé pub et cle prive

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,9 +2,24 @@ const router = require('express').Router();
 const User = require('../models/user.model');
 // librairie qui permet d'encrypter des passwords
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 router.post('/signin', (req,res)=>{
     console.log('req.body');
+    User.findOne({'email': req.body.email}).exec( (err,user) => {
+        // user.password de la bdd
+        // req.body.password du formulaire
+        if(user && bcrypt.compareSync(req.body.password,user.password)){
+            const token = jwt.sign({},'secret',{
+                //header
+                algorithm:'RS256',
+                subject: user._id.toString()
+            })
+        }else{
+            res.status(401).json('signin fail ! email fail');// exception unauthorized on essaye d'accer a une ressource
+            // et on est pas authorisé à le faire
+        }
+    })
     res.json('signin OK !')
 });
 
